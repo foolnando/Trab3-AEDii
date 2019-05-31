1daee873fe8a48dfa55b99a98d9c34f86c8191d9
foolnando <47675174+foolnando@users.noreply.github.com>
2019-05-31 16:55:31 -0400

    Add files via upload

--- Trab3-AEDii/HASH.C
+++ Trab3-AEDii/HASH.C
@@ -36,3 +36,87 @@ void liberaHash(hash* h){
 int chaveDivisao(int chave, int tableSize){
   return(chave & 0x7FFFFFFF) % tableSize;
 }
+
+int chaveMultiplica(int chave, int tableSize){
+  float a = 0.618033898;
+  float val = chave*a;
+  val = val - (int)val;
+  return (int) (tableSize*val)
+}
+
+int insereHash_semColisao(hash* h, Produto *p){
+  if(h == NULL || h->qtd == h->tableSize){
+    return 0;
+  }
+  int chave = p.id;
+  int pos = chaveDivisao(chave, h->tableSize);
+  Produto *new;
+  new = (Produto*) malloc(sizeof(Produto));
+  if(new == NULL){
+    return 0;
+  }
+  new = p;
+  h->item[pos] = novo
+  h->qtd++;
+  return 1;
+}
+
+int buscaHash_semColisao(hash* h, int id, Produto *p){
+  if(h == NULL){
+    return 0;
+  }
+  int pos = chaveDivisao(id, h->tableSize);
+  if(h->item[pos] == NULL){
+    return 0;
+  }
+  *p = *(h->item[pos])
+  return 1;
+}
+
+
+int sondagemLinear(int pos,int i, int tableSize){
+  return ((pos + i) & 0x7FFFFFFF) & tableSize;
+}
+
+int insereHash_enderAberto(hash* h, Produto *p){
+  if(h == NULL || h->qtd == h->tableSize){
+    return 0;
+  }
+  int chave = p.id;
+  int i, pos, newPos;
+  pos = chaveDivisao(chave, h->tableSize);
+  for(i=0;i<h->tableSize;i++){
+    newPos = sondagemLinear(pos,i, h->tableSize);
+    if(h->item[newPos] == NULL){
+      Produto *new;
+      new = (Produto*) malloc(sizeof(Produto));
+      if(novo == NULL){
+        return 0;
+      }
+      *novo = p;
+      h->item[newPos] = novo;
+      h->qtd++;
+      return 1;
+    }
+    return 0;
+  }
+}
+int buscaHash_enderAberto(hash* h, int id, Produto *p){
+  if(h == NULL){
+    return 0;
+  }
+  int i,pos, newPos;
+  int pos = chaveDivisao(id, h->tableSize);
+  for(i=0;i < h->tableSize; i++){
+    newPos = sondagemLinear(pos,i,h->tableSize);
+    if(h->item[newPos] == NULL){
+      return 0;
+    }
+    if(h->item[newPos]->id == id){
+      *p = *(h->item[newPos]);
+      return 1;
+    }
+  }
+  return 0;
+
+}
--- Trab3-AEDii/HASH.h
+++ Trab3-AEDii/HASH.h
@@ -15,6 +15,14 @@ typedef struct hash{
   Produto **item;
 }hash;
 
+hash* criaHash(int tableSize); //cria tabela hash
+void liberaHash(hash* h); //esvazia tabela e destroi
+int chaveDivisao(int chave, int tableSize); //calculo da insercao do produto na tabela
+int chaveMultiplica(int chave, int tableSize); //outro metodo do calculo da insercao
+int insereHash_semColisa(hash* h, Produto p); //insere produto
+int sondagemLinear(int pos,int i, int tableSize); //tratamento de colisao
+  int buscaHash_enderAberto(hash* h, int id, Produto *p); 
+
 
 
 #endif
