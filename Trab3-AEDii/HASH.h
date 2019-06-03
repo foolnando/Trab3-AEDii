e32965b3ac6fca02dacc22db0a994a8ee4ff9a22
foolnando <47675174+foolnando@users.noreply.github.com>
2019-06-03 16:52:32 -0400

    Update HASH.h

--- Trab3-AEDii/HASH.h
+++ Trab3-AEDii/HASH.h
@@ -19,9 +19,9 @@ hash* criaHash(int tableSize); //cria tabela hash
 void liberaHash(hash* h); //esvazia tabela e destroi
 int chaveDivisao(int chave, int tableSize); //calculo da insercao do produto na tabela
 int chaveMultiplica(int chave, int tableSize); //outro metodo do calculo da insercao
-int insereHash_semColisa(hash* h, Produto p); //insere produto
-int sondagemLinear(int pos,int i, int tableSize); //tratamento de colisao
-  int buscaHash_enderAberto(hash* h, int id, Produto *p); 
+int sondagemLinear(int pos,int i, int tableSize); //sondagem linear para colisa
+int insereHash_enderAberto(hash* h, Produto *p); //insere produto colisao
+int buscaHash_enderAberto(hash* h, int id, Produto *p); //busca produto colisao
 
 
 
